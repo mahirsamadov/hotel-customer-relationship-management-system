e5239ab257c418a4a57a6f7e64afe40283260c7b
tahirdibirovresearch <tahirdibirovresearch@gmail.com>
2023-04-15 11:37:32 +0400

    Some hoc and routes has been configured

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,25 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Layout } from "../hoc/Layout";
 import { RequireAuth } from "../hoc/RequireAuth";
 import { RoomDetails } from "../pages/roomDetails";
 import { Rooms } from "../pages/rooms";
 import { UserLogin } from "../pages/userLogin";
 
-const routes = createBrowserRouter(createRoutesFromElements([
-    <Route path="/" element={<RequireAuth/>} >
-        <Route path="/" element={<Layout/>}>
-            <Route path="/rooms" element={<Rooms/>}>
-                <Route path="/:id" element={<RoomDetails/>} />
-            </Route>
-        </Route>
+const routes = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<RequireAuth />}>
+      <Route path="/" element={<Layout />}>
+        <Route path="/rooms/" element={<Rooms />} />
+        <Route path="/rooms/:id" element={<RoomDetails />} />
+      </Route>
     </Route>,
-    <Route path="/login" element={<UserLogin/>} />,
-    <Route path="*" element={<div className="text-white">404</div>} />
-]))
+    <Route path="/login" element={<UserLogin />} />,
+    <Route path="*" element={<div className="text-white">404</div>} />,
+  ])
+);
 
 export default routes;
