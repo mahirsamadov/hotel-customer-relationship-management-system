f25a5075455790ec443a519416a26cce762636ee
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-04-18 12:34:30 +0400

    Filters have been setted up

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid } from '@mui/material'
-import { Button, Pagination, Table } from 'antd'
-import React, { useEffect } from 'react'
+import { Button, Pagination, Table, Checkbox } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { PuffLoader } from 'react-spinners'
@@ -11,6 +11,7 @@ import Selector from '../store/Selector'
 import { Spinner } from '../UI/Spinner'
 
 export const Rooms = () => {
+  const [filter, setFilter] = useState(false)
   const dispatch = useDispatch()
   const navigate = useNavigate()
   useEffect(() => {
@@ -21,6 +22,8 @@ export const Rooms = () => {
   const roomsState = useSelector(Selector.getRoomsState)
   console.log(rooms)
   if(roomsState.loading) return <Spinner/>
+
+  const filteredRooms = filter ? rooms.filter((room: IRoom) => room.guest === '') : rooms
 
   const columns = [
    
@@ -44,6 +47,7 @@ export const Rooms = () => {
         title: 'Price',
         dataIndex: 'price',
         key: 'price',
+        
       },
       {
         title: 'Occupancy',
@@ -68,8 +72,13 @@ export const Rooms = () => {
 
   return (
    
-   <Box className='overflow-y-scroll flex justify-center items-center'>
-   <Table className='w-[80vw] h-[80vh] text-center' size="large" dataSource={rooms} columns={columns} rowKey="id"/>
+   <Box className='overflow-y-scroll flex justify-center items-center flex-col'>
+   <Box className="controller w-[80vw] mb-3">
+   <Button className='bg-blue-500 text-white hover:bg-white' onClick={()=>setFilter(false)}>Clear filters</Button>
+    <Checkbox onChange={()=>setFilter(!filter)} className='m-2'/>
+    <span>Free rooms only</span>
+   </Box>
+   <Table className='w-[80vw] h-[75vh] text-center' size="large" dataSource={filteredRooms} columns={columns} rowKey="id"/>
    </Box>
   )   
 }
