c05b691fdd665e2f49cbf4dd72ba322ccfd741e3
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-04-18 11:37:53 +0400

    idk

--- src/pages/rooms.tsx
+++ src/pages/rooms.tsx
@@ -1,9 +1,11 @@
-import { Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
+import { Button, Pagination, Table } from 'antd'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { PuffLoader } from 'react-spinners'
 import ActionAreaCard from '../components/Card'
+import { IRoom } from '../models'
 import { fetchDataRequest } from '../store/actionCreators'
 import Selector from '../store/Selector'
 import { Spinner } from '../UI/Spinner'
@@ -17,16 +19,57 @@ export const Rooms = () => {
 
   const rooms = useSelector(Selector.getRooms)
   const roomsState = useSelector(Selector.getRoomsState)
-
+  console.log(rooms)
   if(roomsState.loading) return <Spinner/>
 
+  const columns = [
+   
+    {
+      title: 'Number',
+      dataIndex: 'number',
+      key: 'number',
+    },
+
+      {
+        title: 'Type',
+        dataIndex: 'type',
+        key: 'type',
+      },
+      {
+        title: 'Guest',
+        dataIndex: 'guest',
+        key: 'guest',
+      },
+      {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price',
+      },
+      {
+        title: 'Occupancy',
+        dataIndex: 'occupancy',
+        key: 'occupancy',
+      },
+      {
+        title: 'CheckInDate',
+        dataIndex: 'checkInDate',
+        key: 'checkInDate',
+      },
+
+      {
+        title: '',
+        dataIndex: 'id',
+        key: 'id',
+        render: (key:any) => <Button className='bg-blue-500 hover:bg-white text-white' onClick={()=>{navigate(key)}}>More Information</Button>
+      },
+
+]
+
+
   return (
-   <Grid container spacing={4} overflow='scroll' gap={10} padding='5rem' alignItems="center" justifyContent="center" style={{ height: '100vh' }}>{
-   Array.isArray(rooms) && Boolean(rooms.length) && rooms.map((room) => (
-      <div key={room.id} onClick={()=>{navigate(`${room.id}`)}}>
-        <ActionAreaCard image={room.gallery[0]} description={room.description.slice(0, 180) + '...'} type={room.type}/>
-      </div>
-    ))}
-   </Grid>
-  )
+   
+   <Box className='overflow-y-scroll flex justify-center items-center'>
+   <Table className='w-[80vw] h-[80vh] text-center' size="large" dataSource={rooms} columns={columns} rowKey="id"/>
+   </Box>
+  )   
 }
